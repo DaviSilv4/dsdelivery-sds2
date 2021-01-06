cab13e8ef49cbfe2ffe148d010bd41e60d08789a
DaviSilv4 <david.br963@gmail.com>
2021-01-06 10:38:43 -0300

    Projeto criado

---
+++ backend/src/main/java/com/davidSil/DeliverySil/DeliverySilApplication.java
@@ -0,0 +1,13 @@
+package com.davidSil.DeliverySil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliverySilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliverySilApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/davidSil/DeliverySil/DeliverySilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davidSil.DeliverySil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliverySilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
